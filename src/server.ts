dcb967a04e8d45f574ee821b55c1a481223c4a7f
ThomasB013 <thomasbroekman2002@gmail.com>
2026-02-08 23:18:37 +0100

    helloworld grpc server

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import * as grpc from "@grpc/grpc-js";
+import * as protoLoader from "@grpc/proto-loader";
+import * as path from "path";
+
+const PROTO_PATH = path.join(__dirname, "../src/hello.proto");
+const PORT = "0.0.0.0:50051";
+
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+});
+
+const helloProto = grpc.loadPackageDefinition(packageDefinition).hello as any;
+
+function sayHello(
+  call: grpc.ServerUnaryCall<{ name: string }, { message: string }>,
+  callback: grpc.sendUnaryData<{ message: string }>,
+) {
+  const name = call.request.name || "world";
+  callback(null, { message: `Hello, ${name}! 👋` });
+}
+
+const server = new grpc.Server();
+
+server.addService(helloProto.HelloService.service, { SayHello: sayHello });
+server.bindAsync(PORT, grpc.ServerCredentials.createInsecure(), () => {
+  console.log(`🚀 gRPC server running on ${PORT}`);
+});
